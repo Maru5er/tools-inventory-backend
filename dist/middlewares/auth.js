9781601f33ea4149bee0499518bdcdae5a9486d3
Marco Hendra Dani <marcohendradani@gmail.com>
2023-09-09 14:04:46 +0700

    fixed auth error

--- dist/middlewares/auth.js
+++ dist/middlewares/auth.js
@@ -11,9 +11,9 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 let protect = asyncHandler((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     let token = req.headers.authorization;
-    if (token) {
+    if (token && token.startsWith('Bearer')) {
         try {
-            let decoded = jwt.verify(token.split(",")[1], process.env.JWT_SECRET);
+            let decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
             req.user = decoded;
             next();
         }
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,9 +4,9 @@ import asyncHandler from 'express-async-handler';
 
 let protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     let token = req.headers.authorization;
-    if (token) {
+    if (token && token.startsWith('Bearer')) {
         try {
-            let decoded = jwt.verify(token.split(",")[1], process.env.JWT_SECRET);
+            let decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
             req.user = decoded;
             next();
         } catch {
