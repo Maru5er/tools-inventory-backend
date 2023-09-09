b910daf7cc2d2d0a93da6233c51b85f2e6e301cc
Marco Hendra Dani <marcohendradani@gmail.com>
2023-09-09 14:00:55 +0700

    token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 import asyncHandler from 'express-async-handler';
 
 let protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    let token = req.cookie.token;
+    let token = req.headers.authorization;
     if (token) {
         try {
-            let decoded = jwt.verify(token, process.env.JWT_SECRET);
+            let decoded = jwt.verify(token.split(",")[1], process.env.JWT_SECRET);
             req.user = decoded;
             next();
         } catch {
